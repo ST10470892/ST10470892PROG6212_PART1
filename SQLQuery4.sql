098ccd59cdd7796e54640db2e48c1b09c503d60e
Faizaan Mahomed <ST10470892@rcconnect.edu.za>
2026-09-04 19:03:00 +0200

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,9 @@
+USE RaceDayDB;
+
+SELECT * FROM Organiser;
+SELECT * FROM Participant;
+SELECT * FROM Event;
+SELECT * FROM Category;
+SELECT * FROM Enrolment;
+SELECT * FROM Result;
+SELECT * FROM EventSponsor;
